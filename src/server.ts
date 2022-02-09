abfcc3cc5052072a0ac43caf7b3e2ccc4ea14582
HuyTQ <huytq3103@gmail.com>
2022-02-09 17:04:42 +0700

    validate env

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import { validateEnv } from "./core/utils";
 import "dotenv/config";
 import App from "./app";
 import { IndexRoute } from "./modules/index";
+
+validateEnv();
 
 const routes = [new IndexRoute()];
 const app = new App(routes);
