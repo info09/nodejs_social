20b5c1369fc923a78328450729f6fae44ff92911
HuyTQ <huytq3103@gmail.com>
2022-02-09 16:47:29 +0700

    add middleware

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,30 @@
-import { Route } from "core/interfaces";
+import { Route } from "./core/interfaces";
 import express from "express";
 import mongoose from "mongoose";
+import hpp from "hpp";
+import morgan from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import { Logger } from "./core/utils";
 
 class App {
   public app: express.Application;
   public port: string | number;
+  public production: boolean;
 
   constructor(routes: Route[]) {
     this.app = express();
     this.port = process.env.PORT || 5000;
+    this.production = process.env.NODE_ENV == "production" ? true : false;
 
+    this.initializeMiddleware();
     this.initializeRoutes(routes);
     this.connectToDatabase();
   }
 
   public listen() {
     this.app.listen(this.port, () => {
-      console.log(`Server is listening on port ${this.port}`);
+      Logger.info(`Server is listening on port ${this.port}`);
     });
   }
 
@@ -29,14 +37,24 @@ class App {
   private connectToDatabase() {
     const connectString = process.env.MONGODB_URI;
     if (!connectString) {
-      console.log("ConnectString is invalid");
+      Logger.info("ConnectString is invalid");
       return;
     }
-    try {
-      mongoose.connect(connectString);
-      console.log("Database connected...");
-    } catch (error) {
-      console.log("Connect to database error");
+    mongoose.connect(connectString).catch((reason) => {
+      Logger.error(reason);
+    });
+    Logger.info("Database connected...");
+  }
+
+  private initializeMiddleware() {
+    if (this.production) {
+      this.app.use(hpp());
+      this.app.use(helmet());
+      this.app.use(morgan("combined"));
+      this.app.use(cors({ origin: "your.domain.com", credentials: true }));
+    } else {
+      this.app.use(morgan("dev"));
+      this.app.use(cors({ origin: true, credentials: true }));
     }
   }
 }
