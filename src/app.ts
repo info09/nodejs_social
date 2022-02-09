8c34e698c10b7700def2cf5f1a20578d072ee7e1
HuyTQ <huytq3103@gmail.com>
2022-02-09 15:29:51 +0700

    connect database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { Route } from "core/interfaces";
 import express from "express";
+import mongoose from "mongoose";
+
 class App {
   public app: express.Application;
   public port: string | number;
@@ -9,6 +11,7 @@ class App {
     this.port = process.env.PORT || 5000;
 
     this.initializeRoutes(routes);
+    this.connectToDatabase();
   }
 
   public listen() {
@@ -21,6 +24,17 @@ class App {
     routes.forEach((route) => {
       this.app.use("/", route.router);
     });
+  }
+
+  private connectToDatabase() {
+    const connectString =
+      "mongodb://admin:Admin%40123@localhost:27017/nodejs_social?authSource=admin";
+    try {
+      mongoose.connect(connectString);
+      console.log("Database connected...");
+    } catch (error) {
+      console.log("Connect to database error");
+    }
   }
 }
 
